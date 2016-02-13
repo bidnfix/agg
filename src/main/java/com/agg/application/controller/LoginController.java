35e655748d9be2b44b6602ba7d01fabe73135033
AchiniS <AchiniS@Achini>
2016-02-13 14:56:48 +0530

    view resolver changes

--- src/main/java/com/agg/application/controller/LoginController.java
+++ src/main/java/com/agg/application/controller/LoginController.java
@@ -1,9 +1,12 @@
 package com.agg.application.controller;
 
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,7 +16,7 @@ import com.agg.application.model.LoginForm;
 import com.agg.application.model.Result;
 import com.agg.application.service.LoginService;
 
-@RestController
+@Controller
 @RequestMapping("/agg/api/v1")
 public class LoginController {
 
@@ -23,6 +26,11 @@ public class LoginController {
 	@RequestMapping(value = "/login", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Result login(@Valid @RequestBody LoginForm loginForm) {
 		return new Result("success", null, "logged in");
+	}
+	
+	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
+	public String welcome(Map<String, Object> model) {
+		return "index";
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE)
