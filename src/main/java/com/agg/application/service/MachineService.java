f88e16b1fadeee5004e4964fda276c3118071a23
Rajib <rajib.kr@gmail.com>
2016-02-26 13:26:43 +0530

    NA

---
+++ src/main/java/com/agg/application/service/MachineService.java
@@ -0,0 +1,9 @@
+package com.agg.application.service;
+
+import com.agg.application.entity.Manufacturer;
+
+public interface MachineService {
+
+	Iterable<Manufacturer> getManufacturerDetails();
+
+}
