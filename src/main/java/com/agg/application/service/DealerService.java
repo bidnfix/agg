5480b1af2ade9a56313560f49bdc957ea7c670a0
AchiniS <AchiniS@Achini>
2016-03-27 22:10:21 +0530

    dealer changes

--- src/main/java/com/agg/application/service/DealerService.java
+++ src/main/java/com/agg/application/service/DealerService.java
@@ -2,17 +2,28 @@ package com.agg.application.service;
 
 import java.util.List;
 
+import com.agg.application.model.AccountDO;
 import com.agg.application.model.DealerDO;
 import com.agg.application.model.LocationDO;
+import com.agg.application.model.RoleDO;
+import com.agg.application.model.UserDO;
 
 public interface DealerService {
 
 	public List<DealerDO> getDealers();
 
-	public long saveDealer(DealerDO dealerDO);
+	public long saveDealer(DealerDO dealerDO, AccountDO accountDO);
 
 	public DealerDO getDealer(long dealerId);
 
 	public long saveLocation(LocationDO locationDO);
+
+	public List<RoleDO> getDealerAdminRoles();
+	
+	public List<RoleDO> getDealerRoles();
+
+	public List<LocationDO> getDealerLocations(long dealerId);
+
+	public long saveDealerUser(UserDO userDO, AccountDO accountDO);
 
 }
