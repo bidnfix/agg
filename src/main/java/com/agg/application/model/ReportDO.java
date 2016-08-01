a76a1049b9101d9a7515f8034da746b6c554f61b
AchiniS <AchiniS@Achini>
2016-08-01 20:47:27 +0530

    Quote creation changes

---
+++ src/main/java/com/agg/application/model/ReportDO.java
@@ -0,0 +1,501 @@
+package com.agg.application.model;
+
+public class ReportDO {
+	
+	private String dealerName;
+	
+	private String quoteDate;
+	
+	private String attn;
+	
+	private String quoteExpires;
+	
+	private String quoteId;
+	
+	private String address;
+	
+	private String outStandingDesc;
+	
+	private String manufacturerName;
+	
+	private String modelName;
+	
+	private String modelSerialNo;
+	
+	private String equipment;
+	
+	private String retailPrice;
+	
+	private String currentHours;
+	
+	private String machineStatus;
+	
+	private String email;
+	
+	private String phone;
+	
+	private String coverageDesc;
+	
+	private int coverageTerm;
+	
+	private int coverageHours;
+	
+	private String deductibleAmount;
+	
+	private String coverageType;
+	
+	private int powerTrainMonths;
+	
+	private int powerTrainHours;
+	
+	private int hydraulicMonths;
+	
+	private int hydraulicHours;
+	
+	private int fullMachineMonths;
+	
+	private int fullMachineHours;
+	
+	private String warrantyEndDate;
+	
+	private String limitOfLiability;
+	
+	private String price;
+	
+	private String specialConsiderationDesc;
+
+	/**
+	 * @return the dealerName
+	 */
+	public String getDealerName() {
+		return dealerName;
+	}
+
+	/**
+	 * @param dealerName the dealerName to set
+	 */
+	public void setDealerName(String dealerName) {
+		this.dealerName = dealerName;
+	}
+
+	/**
+	 * @return the quoteDate
+	 */
+	public String getQuoteDate() {
+		return quoteDate;
+	}
+
+	/**
+	 * @param quoteDate the quoteDate to set
+	 */
+	public void setQuoteDate(String quoteDate) {
+		this.quoteDate = quoteDate;
+	}
+
+	/**
+	 * @return the attn
+	 */
+	public String getAttn() {
+		return attn;
+	}
+
+	/**
+	 * @param attn the attn to set
+	 */
+	public void setAttn(String attn) {
+		this.attn = attn;
+	}
+
+	/**
+	 * @return the quoteExpires
+	 */
+	public String getQuoteExpires() {
+		return quoteExpires;
+	}
+
+	/**
+	 * @param quoteExpires the quoteExpires to set
+	 */
+	public void setQuoteExpires(String quoteExpires) {
+		this.quoteExpires = quoteExpires;
+	}
+
+	/**
+	 * @return the quoteId
+	 */
+	public String getQuoteId() {
+		return quoteId;
+	}
+
+	/**
+	 * @param quoteId the quoteId to set
+	 */
+	public void setQuoteId(String quoteId) {
+		this.quoteId = quoteId;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/**
+	 * @return the outStandingDesc
+	 */
+	public String getOutStandingDesc() {
+		return outStandingDesc;
+	}
+
+	/**
+	 * @param outStandingDesc the outStandingDesc to set
+	 */
+	public void setOutStandingDesc(String outStandingDesc) {
+		this.outStandingDesc = outStandingDesc;
+	}
+
+	/**
+	 * @return the manufacturerName
+	 */
+	public String getManufacturerName() {
+		return manufacturerName;
+	}
+
+	/**
+	 * @param manufacturerName the manufacturerName to set
+	 */
+	public void setManufacturerName(String manufacturerName) {
+		this.manufacturerName = manufacturerName;
+	}
+
+	/**
+	 * @return the modelName
+	 */
+	public String getModelName() {
+		return modelName;
+	}
+
+	/**
+	 * @param modelName the modelName to set
+	 */
+	public void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
+
+	/**
+	 * @return the modelSerialNo
+	 */
+	public String getModelSerialNo() {
+		return modelSerialNo;
+	}
+
+	/**
+	 * @param modelSerialNo the modelSerialNo to set
+	 */
+	public void setModelSerialNo(String modelSerialNo) {
+		this.modelSerialNo = modelSerialNo;
+	}
+
+	/**
+	 * @return the equipment
+	 */
+	public String getEquipment() {
+		return equipment;
+	}
+
+	/**
+	 * @param equipment the equipment to set
+	 */
+	public void setEquipment(String equipment) {
+		this.equipment = equipment;
+	}
+
+	/**
+	 * @return the retailPrice
+	 */
+	public String getRetailPrice() {
+		return retailPrice;
+	}
+
+	/**
+	 * @param retailPrice the retailPrice to set
+	 */
+	public void setRetailPrice(String retailPrice) {
+		this.retailPrice = retailPrice;
+	}
+
+	/**
+	 * @return the currentHours
+	 */
+	public String getCurrentHours() {
+		return currentHours;
+	}
+
+	/**
+	 * @param currentHours the currentHours to set
+	 */
+	public void setCurrentHours(String currentHours) {
+		this.currentHours = currentHours;
+	}
+
+	/**
+	 * @return the machineStatus
+	 */
+	public String getMachineStatus() {
+		return machineStatus;
+	}
+
+	/**
+	 * @param machineStatus the machineStatus to set
+	 */
+	public void setMachineStatus(String machineStatus) {
+		this.machineStatus = machineStatus;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the phone
+	 */
+	public String getPhone() {
+		return phone;
+	}
+
+	/**
+	 * @param phone the phone to set
+	 */
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	/**
+	 * @return the coverageDesc
+	 */
+	public String getCoverageDesc() {
+		return coverageDesc;
+	}
+
+	/**
+	 * @param coverageDesc the coverageDesc to set
+	 */
+	public void setCoverageDesc(String coverageDesc) {
+		this.coverageDesc = coverageDesc;
+	}
+
+	/**
+	 * @return the coverageTerm
+	 */
+	public int getCoverageTerm() {
+		return coverageTerm;
+	}
+
+	/**
+	 * @param coverageTerm the coverageTerm to set
+	 */
+	public void setCoverageTerm(int coverageTerm) {
+		this.coverageTerm = coverageTerm;
+	}
+
+	/**
+	 * @return the coverageHours
+	 */
+	public int getCoverageHours() {
+		return coverageHours;
+	}
+
+	/**
+	 * @param coverageHours the coverageHours to set
+	 */
+	public void setCoverageHours(int coverageHours) {
+		this.coverageHours = coverageHours;
+	}
+
+	/**
+	 * @return the deductibleAmount
+	 */
+	public String getDeductibleAmount() {
+		return deductibleAmount;
+	}
+
+	/**
+	 * @param deductibleAmount the deductibleAmount to set
+	 */
+	public void setDeductibleAmount(String deductibleAmount) {
+		this.deductibleAmount = deductibleAmount;
+	}
+
+	/**
+	 * @return the coverageType
+	 */
+	public String getCoverageType() {
+		return coverageType;
+	}
+
+	/**
+	 * @param coverageType the coverageType to set
+	 */
+	public void setCoverageType(String coverageType) {
+		this.coverageType = coverageType;
+	}
+
+	/**
+	 * @return the powerTrainMonths
+	 */
+	public int getPowerTrainMonths() {
+		return powerTrainMonths;
+	}
+
+	/**
+	 * @param powerTrainMonths the powerTrainMonths to set
+	 */
+	public void setPowerTrainMonths(int powerTrainMonths) {
+		this.powerTrainMonths = powerTrainMonths;
+	}
+
+	/**
+	 * @return the powerTrainHours
+	 */
+	public int getPowerTrainHours() {
+		return powerTrainHours;
+	}
+
+	/**
+	 * @param powerTrainHours the powerTrainHours to set
+	 */
+	public void setPowerTrainHours(int powerTrainHours) {
+		this.powerTrainHours = powerTrainHours;
+	}
+
+	/**
+	 * @return the hydraulicMonths
+	 */
+	public int getHydraulicMonths() {
+		return hydraulicMonths;
+	}
+
+	/**
+	 * @param hydraulicMonths the hydraulicMonths to set
+	 */
+	public void setHydraulicMonths(int hydraulicMonths) {
+		this.hydraulicMonths = hydraulicMonths;
+	}
+
+	/**
+	 * @return the hydraulicHours
+	 */
+	public int getHydraulicHours() {
+		return hydraulicHours;
+	}
+
+	/**
+	 * @param hydraulicHours the hydraulicHours to set
+	 */
+	public void setHydraulicHours(int hydraulicHours) {
+		this.hydraulicHours = hydraulicHours;
+	}
+
+	/**
+	 * @return the fullMachineMonths
+	 */
+	public int getFullMachineMonths() {
+		return fullMachineMonths;
+	}
+
+	/**
+	 * @param fullMachineMonths the fullMachineMonths to set
+	 */
+	public void setFullMachineMonths(int fullMachineMonths) {
+		this.fullMachineMonths = fullMachineMonths;
+	}
+
+	/**
+	 * @return the fullMachineHours
+	 */
+	public int getFullMachineHours() {
+		return fullMachineHours;
+	}
+
+	/**
+	 * @param fullMachineHours the fullMachineHours to set
+	 */
+	public void setFullMachineHours(int fullMachineHours) {
+		this.fullMachineHours = fullMachineHours;
+	}
+
+	/**
+	 * @return the warrantyEndDate
+	 */
+	public String getWarrantyEndDate() {
+		return warrantyEndDate;
+	}
+
+	/**
+	 * @param warrantyEndDate the warrantyEndDate to set
+	 */
+	public void setWarrantyEndDate(String warrantyEndDate) {
+		this.warrantyEndDate = warrantyEndDate;
+	}
+
+	/**
+	 * @return the limitOfLiability
+	 */
+	public String getLimitOfLiability() {
+		return limitOfLiability;
+	}
+
+	/**
+	 * @param limitOfLiability the limitOfLiability to set
+	 */
+	public void setLimitOfLiability(String limitOfLiability) {
+		this.limitOfLiability = limitOfLiability;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public String getPrice() {
+		return price;
+	}
+
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	/**
+	 * @return the specialConsiderationDesc
+	 */
+	public String getSpecialConsiderationDesc() {
+		return specialConsiderationDesc;
+	}
+
+	/**
+	 * @param specialConsiderationDesc the specialConsiderationDesc to set
+	 */
+	public void setSpecialConsiderationDesc(String specialConsiderationDesc) {
+		this.specialConsiderationDesc = specialConsiderationDesc;
+	}
+	
+}
