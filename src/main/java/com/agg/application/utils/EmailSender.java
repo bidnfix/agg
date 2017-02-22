d51b8d0da18d53a9c8a3e4d7ad6c313cbaf79712
AchiniS <AchiniS@Achini>
2017-02-22 22:41:09 +0530

    fromEmail added

--- src/main/java/com/agg/application/utils/EmailSender.java
+++ src/main/java/com/agg/application/utils/EmailSender.java
@@ -7,6 +7,7 @@ import javax.mail.internet.MimeMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
@@ -23,6 +24,9 @@ public class EmailSender {
     
     @Autowired
     private TemplateEngine templateEngine;
+    
+    @Value("${spring.mail.username}")
+	private String fromEmail;
  
     public EmailStatus sendMailAsText(String to, String subject, String text) {
         return sendMail(to, subject, text, false, null, null);
@@ -51,6 +55,7 @@ public class EmailSender {
         try {
             MimeMessage mail = javaMailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(mail, true);
+            helper.setFrom(fromEmail);
             helper.setTo(InternetAddress.parse(to));
             helper.setSubject(subject);
             helper.setText(text, isHtml);
