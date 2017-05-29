88ed0fd80b82918eab37072aef36b3c205e03bff
Rajib <Rajib@my-pc>
2017-05-30 00:12:19 +0530

    Defect 128 - Service drive new old issue

--- src/main/java/com/agg/application/service/impl/ProgramServiceImpl.java
+++ src/main/java/com/agg/application/service/impl/ProgramServiceImpl.java
@@ -275,7 +275,14 @@ public class ProgramServiceImpl implements ProgramService {
 			logger.debug("quoteDO.getDealerDO().getId() "+quoteDO.getDealerDO().getId());
 			logger.debug("accountDO.getDealerId() "+accountDO.getDealerId());
 			
-			quote.setManfExpired((quoteDO.isCoverageExpired())?(byte)1:(byte)0);
+			//quote.setManfExpired((quoteDO.isCoverageExpired())?(byte)1:(byte)0);
+			
+			//logger.debug("quoteDO.getCondition()  "+quoteDO.getCondition());
+			
+			if(quoteDO.getCondition()==0)
+			{
+				quote.setManfExpired((byte)1);
+			}
 			quote.setManfEndDate(quoteDO.getCoverageEndDate());
 			//quote.setManfEndKnown((quoteDO.isCoverageEndDateUnknown())?(byte)1:(byte)0);
 			quote.setManfEndKnown((byte)1);
@@ -336,22 +343,6 @@ public class ProgramServiceImpl implements ProgramService {
 			
 			//purchased status to 4
 			quote.setStatus((byte)4);
-			
-			/*CustomerInfo customerInfo = new CustomerInfo();
-			customerInfo.setQuoteId(quoteId);
-			customerInfo.setAddress(quoteDO.getDealerAddress());
-			customerInfo.setCity(quoteDO.getDealerCity());
-			customerInfo.setEmail(quoteDO.getDealerEmail());
-			customerInfo.setName(quoteDO.getDealerName());
-			customerInfo.setPhone(quoteDO.getDealerPhone());
-			customerInfo.setRemorse((quoteDO.isCustRemorsePeriod())?(byte)1:(byte)0);
-			customerInfo.setState(quoteDO.getDealerState());
-			customerInfo.setUnderstand((quoteDO.isCustUnderstandCoverage())?(byte)1:(byte)0);
-			customerInfo.setZip(quoteDO.getDealerZip());
-			customerInfo.setLastUpdate(new Date());
-			
-			//TODO
-			customerInfoDAO.save(customerInfo);*/
 			
 			quote = quoteDAO.save(quote);
 			
